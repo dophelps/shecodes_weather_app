b30727abfba072bc2d370bdd25a8df548cb52b3b
dophelps <dophelps@gmail.com>
2023-09-17 18:53:04 -0700

    added GitHub url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"> Weather App</div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1> Weather App</h1>
+      <footer>
+        This project is created by Dani Phelps and is
+        <a
+          href="codesandbox.io/s/react-component-forked-coni0d?file=/src/cities.js"
+          target="_blank"
+        >
+          {" "}
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
